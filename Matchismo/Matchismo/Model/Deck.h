104c5d0b96d4bf5ad752656b50d5816707a09073
Chris Hendrix <chendrix@pivotal.io>
2015-06-02 15:03:50 -0700

    Matchismo: Add Deck

---
+++ Matchismo/Matchismo/Model/Deck.h
@@ -0,0 +1,19 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by pivotal on 6/2/15.
+//  Copyright (c) 2015 Chris Hendrix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Card;
+
+@interface Deck : NSObject
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop;
+
+- (Card *)drawRandomCard;
+
+@end
---
+++ Matchismo/Matchismo/Model/Deck.m
@@ -0,0 +1,51 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by pivotal on 6/2/15.
+//  Copyright (c) 2015 Chris Hendrix. All rights reserved.
+//
+
+#import "Deck.h"
+#import "Card.h"
+
+@interface Deck ()
+
+@property (nonatomic, strong) NSMutableArray *cards;
+
+@end
+
+@implementation Deck
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.cards = [NSMutableArray new];
+    }
+    return self;
+}
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+}
+
+- (Card *)drawRandomCard
+{
+    Card *randomCard;
+    
+    if ([self.cards count]) {
+        unsigned index = arc4random() % [self.cards count];
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+    
+    return randomCard;
+}
+
+@end
